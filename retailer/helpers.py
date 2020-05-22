b90f4b3d20d0bc358420c39a399a1d960171ff07
ivjot singh <ivjot@supply.ai>
2020-05-23 03:46:18 +0530

    able to fetch single shipment data

--- retailer/helpers.py
+++ retailer/helpers.py
@@ -18,4 +18,20 @@ def get_access_token(client_id, client_secret):
         return None, response['error_description']
 
 
+def refresh_access_token(shop):
+    try:
+        login_url = "https://login.bol.com"
+        data = {
+            "client_id": shop.client_id,
+            "client_secret": shop.client_secret,
+            "grant_type": "client_credentials",
+        }
+        resp = requests.post(login_url + "/token", auth=(shop.client_id, shop.client_secret), data=data)
+        resp.raise_for_status()
+        shop.access_token = resp.json()['access_token']
+        shop.save()
+        return True
+    except Exception as e:
+        # todo[iv] logging
+        return False
 
--- shipment/helpers.py
+++ shipment/helpers.py
@@ -1,6 +1,50 @@
+import time
+import requests
+import json
+
+from retailer.helpers import refresh_access_token
+from shipment.models import Shipment
+
+
 def get_shipment_ids():
     pass
 
 
 def sync_all_shipments(access_token):
     pass
+
+
+def sync_shipment_by_id(shop, shipment_id=750036379):
+    while True:
+        sync = False
+        try:
+            if Shipment.objects.filter(shipment_id=shipment_id).exists():
+                sync = True
+                return sync
+            shipment_url = f"https://api.bol.com/retailer/shipments/{shipment_id}"
+            headers = {
+                "Authorization": f"Bearer {shop.access_token}",
+                "Accept": "application/vnd.retailer.v3+json",
+            }
+
+            response = requests.get(shipment_url, headers=headers)
+            print(response)
+            print(vars(response))
+            response_data = json.loads(response._content)
+            print(response_data['title'])
+            print()
+            # todo [add 2 attempts]
+            if response_data['title'] == 'Expired JWT' and response_data['status'] == 401:
+                print("coming here baby")
+                refresh_access_token(shop=shop)
+                continue
+            else:
+                sync = True
+
+        except Exception as e:
+            if Exception == '429':
+                time.sleep(60)
+            elif Exception == 'token Exception':
+                refresh_access_token(shop=shop)
+        if sync:
+            break
