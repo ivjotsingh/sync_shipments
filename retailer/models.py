bab1143d4bf6881be8a5f73cf7129693abc98bec
ivjot singh <ivjot@supply.ai>
2020-05-23 02:00:15 +0530

    models added in retailer and shipment

--- retailer/models.py
+++ retailer/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Shop(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    client_id = models.CharField(max_length=51, blank=True, null=True)
+    client_secret = models.CharField(max_length=250, blank=True, null=True)
+    access_token = models.CharField(max_length=250, blank=True, null=True)
+    name = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        db_table = 'shop'
--- shipment/models.py
+++ shipment/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import uuid
+import jsonfield
 
-# Create your models here.
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+from retailer.models import Shop
+
+
+class Shipment(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    shipment_id = models.BigIntegerField(unique=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    pick_up_point = models.BooleanField()
+    shipment_date = models.DateTimeField(null=True)
+    shipment_reference = models.CharField(max_length=50, null=True)
+    fulfilment_method = models.CharField(max_length=7, null=True)
+    shipment_items = jsonfield.JSONField()
+    transport = jsonfield.JSONField()
+    customer_details = jsonfield.JSONField()
+    billing_details = jsonfield.JSONField()
+
+    class Meta:
+        db_table = 'shipment'
