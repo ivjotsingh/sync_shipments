47e4d35ec7c3cc4d46a2e8b35648b78c47eaa57b
ivjotsingh <ivjotofficial@gmail.com>
2020-05-26 02:39:48 +0530

    000

--- sync_shipments.py
+++ sync_shipments.py
@@ -11,36 +11,32 @@ from shipment.helpers import ShipmentSync
 from retailer.models import Shop
 
 
-def pdf_process_function(msg):
-
-    print(" PDF processing")
+def sync_shipments(msg):
     print(" [x] Received " + str(msg))
     shop_id = json.loads(msg)['shop_id']
     sync = ShipmentSync(Shop.objects.get(id=shop_id))
     sync.sync_all_shipments()
-    sync = ShipmentSync(Shop.objects.get(id=shop_id))
-    sync.sync_all_shipments()
-    print(" PDF processing finished")
+    print("Shipments sync finished")
     return
 
 
-# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+# Access the CLODUAMQP_URL environment variable and parse it
 url = config('CLOUDAMQP_URL')
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 channel = connection.channel() # start a channel
 channel.queue_declare(queue='shipments_sync') # Declare a queue
 
+
 # create a function which is called on incoming messages
 def callback(ch, method, properties, body):
-  pdf_process_function(body)
+    sync_shipments(body)
+
 
 # set up subscription on the queue
-channel.basic_consume('shipments_sync',
-  callback,
-  auto_ack=True)
+channel.basic_consume('shipments_sync', callback, auto_ack=True)
+
 
 # start consuming (blocks)
 channel.start_consuming()
-print("ended")
 connection.close()
